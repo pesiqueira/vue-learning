8b4469c8948f4eef44b633484c547c72818eff62
Paulo Enrique Lino de Siqueira <plino@br.ibm.com>
2018-10-11 13:40:39 -0300

    lesson7

--- app.js
+++ app.js
@@ -1,27 +1,15 @@
 new Vue({
     el:'#vue-app',
     data:{
-        age: '25',
-        x: 0,
-        y: 0,
+        name: '',
+        age: ''
     },
     methods:{
-       add: function(inc){
-           this.age = this.age + inc;
-       },
-       sub: function (dec) {
-           this.age = this.age - dec;
-       },
-       updateXY: function(event){
-           this.x = event.offsetX;
-           this.y = event.offsetY;
-           event.offsetX = 0;
-           event.offsetY = 0;
-
-           console.log(event);
-       },
-       click: function(){
-           alert('You clicked me');
-       }
+        logName: function(){
+            console.log('You entered your name');
+        },
+        logAge: function(){
+            console.log('You entered your age');
+        }
     }
 });
