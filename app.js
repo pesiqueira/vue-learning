806fe3e0ef695ee1d40475272970de846ce42fd7
Paulo Enrique Lino de Siqueira <plino@br.ibm.com>
2018-10-22 17:15:25 -0300

    leasson15

--- app.js
+++ app.js
@@ -1,35 +1,26 @@
-var one = new Vue({
-    el:'#vue-app-one',
-    data:{
-        title:'Vue App One'
+var data = {
+    name: 'Yoshi'
+}
+
+Vue.component('greeting',{
+    template: `<p> Hey there, I am {{name}}. <button v-on:click="changeName">Change name</button></p>`,
+    data: function(){
+        return data
+        // {
+        //     name:'Yoshi'
+        // }
     },
     methods:{
-  
-    },
-    computed:{
-        greet: function(){
-            return 'Hello from app one :)';
+        changeName: function(){
+            this.name = 'Mario'
         }
-        
     }
+})
+
+var one = new Vue({
+    el:'#vue-app-one'
 });
 
 var two = new Vue({
-    el:'#vue-app-two',
-    data:{
-        title:'Vue App Two'
-    },
-    methods:{
-        changeTitle: function () {
-            one.title = "Title changed";
-            
-        }
-    },
-    computed:{
-        greet: function(){
-            return 'Hello from app two :)';
-        }
-    }
+    el:'#vue-app-two'
 });
-
-two.title = "Changed from outside"
