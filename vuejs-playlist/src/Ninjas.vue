c75d48da82ab10ed05e1662ae53dcdab234a4592
Paulo Enrique Lino de Siqueira <plino@br.ibm.com>
2018-10-24 18:48:44 -0300

    leasson18

---
+++ vuejs-playlist/src/Ninjas.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>List of ninjas</h1>
+    <ul>
+      <li v-for="ninja in ninjas" :key= "ninja">{{ ninja }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      ninjas: ['Youshi','Mario','Ryu']
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1{
+  color: green
+}
+</style>
