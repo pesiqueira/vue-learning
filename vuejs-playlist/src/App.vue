ba03415c693d9c7dbf89a6f6401973537e3e401e
Paulo Enrique Lino de Siqueira <plino@br.ibm.com>
2018-10-25 09:50:22 -0300

    leasson21

--- vuejs-playlist/src/App.vue
+++ vuejs-playlist/src/App.vue
@@ -1,28 +1,30 @@
 <template>
-  <div id="app">
-    <h1>{{title}}</h1>
-    <ninjas></ninjas>
+  <div>
+    <app-header></app-header>
+    <app-ninjas></app-ninjas>
+    <app-footer></app-footer>
   </div>
 </template>
 
 <script>
-import Ninjas from './Ninjas.vue'
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Ninjas from './components/Ninjas'
 
 export default {
-  name: 'app',
   components:{
-    'ninjas': Ninjas
+    'app-header':Header,
+    'app-footer': Footer,
+    'app-ninjas': Ninjas
   },
   data () {
     return {
-      title: 'Ninja App'
+
     }
   }
 }
 </script>
 
 <style scoped>
-h1{
-  color: purple;
-}
+
 </style>
